4713d738882fc17550c3f5baad10d6c1a5a89f8b
aamonnlimaa@gmail.com <aamonnlimaa@gmail.com>
2025-02-18 12:54:41 -0300

    chore: update css variables

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 const ProductsPage = () => {
   return (
     <div className='p-5 border border-red-500 rounded-xl'>
       <h1>Products</h1>
+      <Button>Click me</Button>
     </div>
   );
 };
