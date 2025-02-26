9afe578302d4313e1a51abf76b0c35a2ba85b257
aamonnlimaa@gmail.com <aamonnlimaa@gmail.com>
2025-02-26 05:47:57 -0300

    feat: add product header component

--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -1,21 +1,29 @@
 import { Product } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
-
+import { useParams } from "next/navigation";
 interface ProductsProps {
   products: Product[];
 }
 
 const Products = ({ products }: ProductsProps) => {
+  const { slug } = useParams<{ slug: string }>();
+
   return (
     <div className="space-y-3 px-5">
-      {products.map(product => (
-        <Link key={product.id} href="/" className="flex items-center justify-between gap-10 border-b py-3">
+      {products.map((product) => (
+        <Link
+          key={product.id}
+          href={`/${slug}/menu/${product.id}`}
+          className="flex items-center justify-between gap-10 border-b py-3"
+        >
           {/* Escquerda */}
           <div>
             <h3 className="text-sm font-medium">{product.name}</h3>
-            <p className="text-sm text-muted-foreground line-clamp-2 ">{product.description}</p>
-            <p className= "pt-3 text-sm font-semibold">
+            <p className="line-clamp-2 text-sm text-muted-foreground">
+              {product.description}
+            </p>
+            <p className="pt-3 text-sm font-semibold">
               {new Intl.NumberFormat("pt-BR", {
                 style: "currency",
                 currency: "BRL",
