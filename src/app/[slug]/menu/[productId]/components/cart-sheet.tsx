6ae6af477045e1ad5768f3a2ec2a224cdb1d9968
aamonnlimaa@gmail.com <aamonnlimaa@gmail.com>
2025-02-26 13:45:31 -0300

    feat: ensure same product can be added more than once to cart

--- src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
+++ src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
@@ -1,31 +1,33 @@
 import { useContext } from "react";
 
-import { Sheet, SheetContent, SheetDescription, SheetHeader,SheetTitle } from "@/components/ui/sheet";
-
-import { CartContext } from "../../contexts/cart";
+import { CartContext } from "@/app/[slug]/menu/contexts/cart";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
 
 const CartSheet = () => {
-  const { isOpen, toggleCart, products } = useContext(CartContext); 
+  const { isOpen, toggleCart, products } = useContext(CartContext);
   return (
-    <>
-      <Sheet open={isOpen} onOpenChange={toggleCart} >
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>Sacola</SheetTitle>
-            <SheetDescription>description</SheetDescription>
-          </SheetHeader>
-          {
-            products.map((product: { id: string; name: string; quantity: number }) => {
-              return (
-                <div key={product.id}>
-                  <h1> {product.name} - {product.quantity} </h1>
-                </div>
-              );
-            })
-          }
-        </SheetContent>
-      </Sheet>
-    </>
+    <Sheet open={isOpen} onOpenChange={toggleCart}>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Are you absolutely sure?</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </SheetDescription>
+        </SheetHeader>
+        {products.map((product) => (
+          <h1 key={product.id}>
+            {product.name} - {product.quantity}
+          </h1>
+        ))}
+      </SheetContent>
+    </Sheet>
   );
 };
 
--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -1,8 +1,10 @@
 "use client";
+
 import { Product } from "@prisma/client";
 import { createContext, ReactNode, useState } from "react";
 
-interface CartProduct extends Pick<Product, "id" | "name" | "price" | "imageUrl"> {
+interface CartProduct
+  extends Pick<Product, "id" | "name" | "price" | "imageUrl"> {
   quantity: number;
 }
 
@@ -22,16 +24,30 @@ export const CartContext = createContext<ICartContext>({
 
 export const CartProvider = ({ children }: { children: ReactNode }) => {
   const [products, setProducts] = useState<CartProduct[]>([]);
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const toggleCart = () => {
     setIsOpen((prev) => !prev);
   };
-
   const addProduct = (product: CartProduct) => {
-    setProducts((prev) => [...prev, product]);
-  }
-
+    const productIsAlreadyOnTheCart = products.some(
+      (prevProduct) => prevProduct.id === product.id,
+    );
+    if (!productIsAlreadyOnTheCart) {
+      return setProducts((prev) => [...prev, product]);
+    }
+    setProducts((prevProducts) => {
+      return prevProducts.map((prevProduct) => {
+        if (prevProduct.id === product.id) {
+          return {
+            ...prevProduct,
+            quantity: prevProduct.quantity + product.quantity,
+          };
+        }
+        return prevProduct;
+      });
+    });
+  };
   return (
     <CartContext.Provider
       value={{
